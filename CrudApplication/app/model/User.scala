77196a795c8f5b76b617d0b2e5e6654e519763e0
Zerouali <oussamazerouali42@gmail.com>
2016-10-13 22:57:59 +0000

    read and write user model

--- CrudApplication/app/model/User.scala
+++ CrudApplication/app/model/User.scala
@@ -8,11 +8,46 @@ import play.api.data._
 import slick.driver.MySQLDriver.api._
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.Play.current
+import model.Tables.{UsersRow}
+
+import play.api.libs.json._
+
+
+import scala.concurrent.Future
+
+import scala.math.BigDecimal
 
 object User {
 	
   val users = Users
   val db = Database.forConfig("mysql");
+
+    implicit object UserFormat extends Format[UsersRow] {
+
+    // convert from Tweet object to JSON (serializing to JSON)
+    def writes(user: UsersRow): JsValue = {
+      //  tweetSeq == Seq[(String, play.api.libs.json.JsString)]
+      val usersSeq = Seq(
+        "id" -> JsNumber(user.id),
+        "code" -> JsString(user.nom.get),
+        "title" -> JsString(user.prenom.get)
+     
+      )
+      JsObject(usersSeq)
+    }
+
+    // convert from JSON string to a Tweet object (de-serializing from JSON)
+    // (i don't need this method; just here to satisfy the api)
+    def reads(userInfo: JsValue): JsResult[UsersRow] = {
+      val id = (userInfo \ "id").asOpt[Int].getOrElse(0)
+      val prenom = (userInfo \ "prenom").asOpt[String]
+      val nom = (userInfo \ "nom").asOpt[String]
+     
+      var userRow = UsersRow(id = id, nom = nom, prenom = prenom)
+
+      JsSuccess(userRow)
+    }
+  }
 
   //find by id 
   def findById(id : Int) = {
